3995d3a4f7ef43b3cbb42803673504ace1e34a11
Serafin Schroff <serafin.schroff@gmail.com>
2026-07-15 17:08:03 +0200

    changed to use command-line arguments, to easily use the tool for any given folder. relative paths should suffice. right now the tool only provides you with the list of files that can be deleted but does not delete them yet. could add a deleting option, which should maybe first prompt the user if they agree to delete the mentioned files.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,21 @@ use regex::escape;
 use regex::Regex;
 use std::fs;
 use std::collections::HashSet;
+use argh::FromArgs;
 
+#[derive(FromArgs)]
+#[argh(description = "{command_name} is a tool to provide directory paths. it is required to provide both --attachments_directory and --vault.")]
+struct Directories {
+    #[argh(option, description = "the directory path for the attachments. relative path should suffice.")]
+    attachments_directory: String,
 
+    #[argh(option, description = "the directory path for the entire vault. relative path should suffice.")]
+    vault: String,
+}
 
-fn get_attachments() -> Vec<String> {
+fn get_attachments(dir: &String) -> Vec<String> {
     let mut attachments: Vec<String> = Vec::new();
-    for entry in WalkDir::new("test-dir/attachments/") {
+    for entry in WalkDir::new(dir) {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             attachments.push(entry.file_name().to_string_lossy().into_owned());
@@ -22,10 +31,10 @@ fn build_regex_string(attachments: &[String]) -> String {
     format!("({})", escaped.join("|"))
 }
 
-fn find_mentioned(re: &Regex) -> HashSet<String> {
+fn find_mentioned(re: &Regex, vault: &String) -> HashSet<String> {
     let mut mentioned = HashSet::new();
 
-    for entry in WalkDir::new("test-dir")
+    for entry in WalkDir::new(vault)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.path().extension().map_or(false, |ext| ext == "md"))
@@ -41,12 +50,13 @@ fn find_mentioned(re: &Regex) -> HashSet<String> {
 }
 
 fn main() {
-    let attachments: Vec<String> = get_attachments();
+    let args: Directories = argh::from_env();
+    let attachments: Vec<String> = get_attachments(&args.attachments_directory);
     println!("attachments: {:#?}", &attachments);
     let pattern = build_regex_string(&attachments);
     println!("regex pattern: {}", &pattern);
     let re = Regex::new(&pattern).unwrap();
-    let mentioned = find_mentioned(&re);
+    let mentioned = find_mentioned(&re, &args.vault);
     println!("mentioned attachments: {:#?}", &mentioned);
     let unmentioned: Vec<&String> = attachments.iter()
         .filter(|a| !mentioned.contains(a.as_str()))
